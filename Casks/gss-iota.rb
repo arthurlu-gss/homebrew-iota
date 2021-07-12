b0fab35d56106f47b4d00dd0f10a01f9fc5357ae
Arthu Lu <arthur_lu@gss.com.tw>
2021-07-12 10:50:27 +0800

    Specify the version number for gss-iota.rb

--- Casks/gss-iota.rb
+++ Casks/gss-iota.rb
@@ -1,8 +1,8 @@
 cask 'gss-iota' do
-  version :latest
-  sha256 :no_check
+  version '1.12.2'
+  sha256 'ea80fcd06a48c6af447ac026e9d2e3915661207b847a19e0b03720edac7de7a9'
 
-  url 'https://im.gss.com.tw/download/iota.dmg',
+  url 'https://gssweb.gss.com.tw/iota/iota.dmg',
       user_agent: :fake
   name 'iota'
   homepage 'https://im.gss.com.tw/'
